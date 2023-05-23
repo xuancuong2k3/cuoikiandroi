435e899ac2841ca8b736648863823bb96d278e9d
xuancuong2k3 <xuancuong2k3@gmail.com>
2023-05-23 20:58:46 +0700

    first commit

--- taskt_mangament/android/app/src/main/kotlin/com/example/taskt_mangament/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.taskt_mangament
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- taskt_mangament/lib/models/task.dart
+++
@@ -1,9 +0,0 @@
-class Task {
-  String title;
-  String description;
-  bool completed;
-  DateTime dateTime;
-
-  Task(
-      {required this.title, required this.description, required this.dateTime, this.completed = false});
-}
